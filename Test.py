9d0c6e761909e061d374eb0d8164c991526bd2c2
Bitchididii <sakhuja.harshit@gmail.com>
2024-04-30 06:37:35 +0530

    Code rectified

--- Test.py
+++ Test.py
@@ -1,5 +1,5 @@
 time=int(0)
 for i in range (5):
-	print("The current build number is",i)
-	print("The time scheduled is",time)
+    print("The current build number is",i)
+    print("The time scheduled is",time)
     time=time+5
